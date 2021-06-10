36b97c9a257010e2f9914281fd798ba2f38573a7
Angela Flowers <me@angela.flowers>
2021-06-10 14:38:13 -0400

    added endpoints

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import re
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -27,6 +28,32 @@ class CarsModel(db.Model):
 @app.route('/')
 def hello():
     return {"hello": "world"}
+
+
+@app.route('/cars', methods=['POST', 'GET'])
+def handle_cars():
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_car = CarsModel(name=data['name'], model=data['model'], doors=data['doors'])
+            db.session.add(new_car)
+            db.session.commit()
+            return {"message": f"car {new_car.name} has been created successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format."}
+
+    elif request.method == 'GET':
+        cars = CarsModel.query.all()
+        results = [
+            {
+                "name": car.name,
+                "model": car.model,
+                "doors": car.doors
+            } for car in cars]
+        
+
+        return {"count": len(results), "cars": results}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
